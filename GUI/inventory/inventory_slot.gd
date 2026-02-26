0865a5ac41fd20c58c25a05bc6660efb0dfd3faf
Gnueysivolc <clovisyeung1234@gmail.com>
2026-02-26 15:47:50 +0800

    done all box and injection animation

--- GUI/inventory/inventory_slot.gd
+++ GUI/inventory/inventory_slot.gd
@@ -7,6 +7,11 @@ var icon: TextureRect
 @export var red_icon: Texture2D
 @export var blue_icon: Texture2D
 @export var green_icon: Texture2D
+@export var purple_icon: Texture2D
+@export var red_injection: Texture2D
+@export var blue_injection: Texture2D
+@export var purple_injection: Texture2D
+@export var green_injection: Texture2D
 
 
 func _ready():
@@ -28,5 +33,15 @@ func set_item(type: ItemTypes.ItemType):
 			icon.texture = blue_icon
 		ItemTypes.ItemType.GREEN_PILL:
 			icon.texture = green_icon
+		ItemTypes.ItemType.PURPLE_PILL:
+			icon.texture = purple_icon
+		ItemTypes.ItemType.RED_INJECTION:
+			icon.texture = red_injection
+		ItemTypes.ItemType.GREEN_INJECTION:
+			icon.texture = green_injection
+		ItemTypes.ItemType.BLUE_INJECTION:
+			icon.texture = blue_injection
+		ItemTypes.ItemType.PURPLE_INJECTION:
+			icon.texture = purple_injection
 		_:
 			icon.texture = null
